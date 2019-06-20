8d904356c2f7971bcf9c250090baf41c270a19da
src8655 <src8655@naver.com>
2019-06-20 13:56:23 +0900

    190620

--- emaillist/views.py
+++ emaillist/views.py
@@ -1,9 +1,17 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 
 # Create your views here.
+from emaillist.models import Emaillist
+
+
 def index(request):
-    return render(request, 'emaillist/index.html')
+    emaillist = Emaillist.objects.all().order_by('-id')
+    data = {
+        'emaillist': emaillist
+    }
+    return render(request, 'emaillist/index.html', data)
 
 
 def form(request):
@@ -11,8 +19,11 @@ def form(request):
 
 
 def add(request):
-    firstname = request.POST['fn']
-    lastname = request.POST['ln']
-    email = request.POST['email']
+    emaillist = Emaillist()
+    emaillist.first_name = request.POST['fn']
+    emaillist.last_name = request.POST['ln']
+    emaillist.email = request.POST['email']
 
-    return render(request, 'emaillist/add.html')
+    emaillist.save()
+
+    return HttpResponseRedirect('/emaillist')
